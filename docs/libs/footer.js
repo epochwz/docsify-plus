4821bd68408c4a03c7916a87f0de6f228ea2fc88
epochwz <epoch.wz@gmail.com>
2019-11-06 13:25:05 +0800

    docsify: initial

---
+++ docs/libs/footer.js
@@ -0,0 +1,9 @@
+window.$docsify = {
+};
+
+const footerHtml = [
+    '<div id="app"></div>',
+    '<script src="//unpkg.com/docsify/lib/docsify.min.js"></script>',
+].join('');
+
+document.write(footerHtml);
