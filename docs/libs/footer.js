96835e2c289c72d8f293503c1ef2e6550b2e311b
epochwz <epoch.wz@gmail.com>
2019-11-08 14:25:29 +0800

    docsify: 插件 -- 全文搜索

--- docs/libs/footer.js
+++ docs/libs/footer.js
@@ -14,11 +14,21 @@ window.$docsify = {
     loadSidebar: 'sidebar.md',
     subMaxLevel: 4,
     autoHeader: true,
+    // 全文搜索
+    search: {
+        maxAge: 86400000, // 过期时间，单位毫秒，默认一天
+        depth: 4, // 搜索标题的最大层级
+        paths: 'auto',
+        placeholder: 'search',
+        noData: 'Not Found!'
+    },
 };
 
 const footerHtml = [
     '<div id="app"></div>',
     '<script src="//unpkg.com/docsify/lib/docsify.min.js"></script>',
+    '<!-- 全文搜索 -->',
+    '<script src="//unpkg.com/docsify/lib/plugins/search.min.js"></script>',
 ].join('');
 
 document.write(footerHtml);
